34f9c9e065e6d11d028c77dacdcc1ea59522a2bc
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2023-07-31 15:11:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import apikey
+import time
+import os
 from prettytable import PrettyTable
 from humanize import intword
 from colored import Fore, Back, Style
@@ -10,35 +12,42 @@ headers = {'Accepts': 'application/json',
 		  'X-CMC_PRO_API_KEY' : apikey.key,
 		  }
 
+limit = 30
+
 params = {'start': 1,
-		 'limit': 20,
+		 'limit': limit,
 		 'convert': 'INR',
 		 }
 
-response = requests.get(url, params=params, headers=headers).json()
-table = PrettyTable()
-table.field_names = ['ID','Rank', 'Name', 'Symbol', 'Price[INR]', '1h%', '24h%', '7d%', 'Market Cap']
+def percent_color(value):
+	if float(value) > 0:
+		return f"{Fore.black}{Back.green} {format(value, '.2f')}{Style.reset}"
+	else:
+		return f"{Fore.white}{Back.red} {format(value, '.2f')}{Style.reset}"
 
-coins = response['data']
+while True:	
+	response = requests.get(url, params=params, headers=headers).json()
 
+	table = PrettyTable()
+	table.field_names = ['ID','Rank', 'Name', 'Symbol', 'Price[INR]', '1h%', '24h%', '7d%', 'Market Cap']
 
-for i in coins:
-	up_1h = f"{Fore.black}{Back.green} {format(i['quote']['INR']['percent_change_1h'], '.2f')}{Style.reset}"
-	down_1h = f"{Fore.white}{Back.red}{format(i['quote']['INR']['percent_change_1h'], '.2f')}{Style.reset}"
-	
-	up_24h = f"{Fore.black}{Back.green} {format(i['quote']['INR']['percent_change_24h'], '.2f')}{Style.reset}"
-	down_24h = f"{Fore.white}{Back.red}{format(i['quote']['INR']['percent_change_24h'], '.2f')}{Style.reset}"
-	
-	up_7d = f"{Fore.black}{Back.green} {format(i['quote']['INR']['percent_change_7d'], '.2f')}{Style.reset}"
-	down_7d = f"{Fore.white}{Back.red}{format(i['quote']['INR']['percent_change_7d'], '.2f')}{Style.reset}"
-	
-	table.add_row([i['id'],
-				   i['cmc_rank'],
-				   i['name'],
-				   i['symbol'],
-				   i['quote']['INR']['price'],
-				   up_1h if float(i['quote']['INR']['percent_change_1h']) > 0 else down_1h,
-				   up_24h if float(i['quote']['INR']['percent_change_24h']) > 0 else down_24h,
-				   up_7d if float(i['quote']['INR']['percent_change_7d']) > 0 else down_7d,
-				   intword(i['quote']['INR']['market_cap'])])
-print(table)
+	coins = response['data']
+	for i in coins:	
+		table.add_row([
+				i['id'],
+				i['cmc_rank'],
+				i['name'],
+				i['symbol'],
+				i['quote']['INR']['price'],
+				percent_color(i['quote']['INR']['percent_change_1h']),
+				percent_color(i['quote']['INR']['percent_change_24h']),
+				percent_color(i['quote']['INR']['percent_change_7d']),
+				intword(i['quote']['INR']['market_cap'])])
+	#Clear the terminal screen
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print(f"{Style.BOLD}{Fore.black}{Back.cyan}CRYPTOCURRENCY STATS{Style.reset}")
+	print(f"{Fore.black}{Back.green}Values are updated every 10 secs{Style.reset}")
+	print(table)
+	print(f"{Fore.white}{Back.red}Ctrl+Z to quit{Style.reset}")
+	#Time to refresh
+	time.sleep(10)
